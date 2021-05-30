03ce660c24865ed97b221247e4bfb6f8287c8b6a
liubin_glb <liubin_glb@liubin.com>
2021-05-30 14:59:48 +0800

    first

---
+++ c++11/c11_test/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string str2 = "D:\hello\world\test.text";
+    cout << str2 << endl;
+
+
+
+    cout << "Hello World!" << endl;
+
+    string str=R"(d:\dsa\dsa\dsa)";
+    cout<<str<<endl;
+
+    string str1 = "d:dsdsds\tdsadsa\mdsads";
+    cout<<str1<<endl;
+        return 0;
+}
